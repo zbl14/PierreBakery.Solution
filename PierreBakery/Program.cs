af104b8dd704e845a220464e113f77ceb685b996
zbl14 <ifthereisoneday@gmail.com>
2022-07-15 21:56:18 -0700

    DRY, BreadOrPastry method, CheckouOrNot

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using PierreBakery.Models;
 
 namespace PierreBakery
@@ -8,19 +9,30 @@ namespace PierreBakery
   {
     public static void Main() 
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("=============================\n");
-      Console.WriteLine("Would you like some bread or some pastries?\n" + 
-        "'B' for Bread, 'P' for pastry, 'Enter' for quit\n");
-      Console.WriteLine("Bread: $5 per/loaf, Buy 2 get 1 free\n");
+      Console.WriteLine(
+        "Welcome to Pierre's Bakery!\n" + 
+        "===========================\n");
+      Thread.Sleep(1000);
+      Console.WriteLine("Would you like some bread or some pastries?\n");
+      Console.WriteLine(
+        "Bread:\n" + 
+        "$5 per/loaf, Buy 2 get 1 free\n");
       Console.WriteLine(
         "Pastry:\n" +
         "Buy 1 for $2\n" + 
         "Buy 3 for $5\n" + 
         "Buy 4 for $7\n" + 
         "Buy 5 for $9\n" +
-        "Buy half-dozen for $10\n");
+        "Buy half-dozen for $10\n\n" +
+        "'B' for Bread\n" +
+        "'P' for Pastry\n" + 
+        "'Enter' for Quit\n");
       string response = Console.ReadLine();
+      BreadOrPastry(response);
+    }
+
+    static void BreadOrPastry(string response)
+    {
       if (response.ToUpper() == "B")
       {
         OrderBread();
@@ -29,9 +41,32 @@ namespace PierreBakery
       {
         OrderPastry();
       }
+      else if (response.ToUpper() == "C")
+      {
+        Checkout();
+      }
       else
       {
         Quit();
+      }
+    }
+
+    static void CheckoutOrNot(string responseCheckOut)
+    {
+      if (responseCheckOut.ToUpper() == "Y")
+      {
+        Checkout();
+      }
+      else if (responseCheckOut.ToUpper() == "N")
+      {
+      Console.WriteLine(
+        "Would you like some bread or some pastries?\n\n" + 
+        "'B' for Bread\n" +
+        "'P' for Pastry\n" + 
+        "'C' for Checkout\n" + 
+        "'Enter' for Quit");
+      string response = Console.ReadLine();
+      BreadOrPastry(response);
       }
     }
 
@@ -43,24 +78,7 @@ namespace PierreBakery
       Console.WriteLine("The total cost of this bread order is $ " + newBreadOrder.BreadOrderCost() + "\n");
       Console.WriteLine("Would you like to check out? 'Y' for yes, 'N' for continue shopping");
       string responseCheckOut = Console.ReadLine();
-      if (responseCheckOut.ToUpper() == "Y")
-      {
-        Checkout();
-      }
-      Console.WriteLine("Would you like to order some pastries or continue to order bread? 'P' for pastry, 'B' for bread, 'Enter' for quit");
-      string response = Console.ReadLine();
-      if (response.ToUpper() == "B")
-      {
-        OrderBread();
-      }
-      else if (response.ToUpper() == "P")
-      {
-        OrderPastry();
-      }
-      else
-      {
-        Quit();
-      }
+      CheckoutOrNot(responseCheckOut);
     }
 
     static void OrderPastry()
@@ -71,27 +89,7 @@ namespace PierreBakery
       Console.WriteLine("The total cost of this pastries order is $ " + newPastryOrder.PastryOrderCost() + "\n");
       Console.WriteLine("Would you like to check out? 'Y' for yes, 'N' for continue shopping");
       string responseCheckOut = Console.ReadLine();
-      if (responseCheckOut.ToUpper() == "Y")
-      {
-        Checkout();
-      }
-      else if (responseCheckOut.ToUpper() == "N")
-      {
-        Console.WriteLine("Would you like to order some bread or continue to order pastry? 'P' for pastry, 'B' for bread, 'Enter' for quit");
-        string response = Console.ReadLine();
-        if (response.ToUpper() == "B")
-        {
-          OrderBread();
-        }
-        else if (response.ToUpper() == "P")
-        {
-          OrderPastry();
-        }
-        else
-        {
-          Quit();
-        }
-      }
+      CheckoutOrNot(responseCheckOut);
     }
 
     static void Checkout()
@@ -117,7 +115,7 @@ namespace PierreBakery
         total = breadTotal + pastryTotal;
         if (breadTotal == 0 || pastryTotal == 0)
         {
-          Console.WriteLine("Your total for order(s) is $ " + total);
+          Console.WriteLine("Your total for order(s) is $ " + total + "\n");
           Quit();
         }
         else
